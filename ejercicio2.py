400860d3062e9bb6efcc3b3c43f47615fe774d8f
andres vargas <anmendez845@gmail.com>
2024-04-18 11:38:19 -0500

    mi segundo y tercer trabajo

---
+++ ejercicio2.py
@@ -0,0 +1,25 @@
+a=int(input("Digite el valor para a: "))
+b=int(input("Digite el valor para a: "))
+suma=a+b
+print("la suma es:  ",suma)
+
+a=int(input("Digite el valor para a: "))
+b=int(input("Digite el valor para a: "))
+resta=a-b
+print("la resta es:  ",resta )
+
+
+a=int(input("Digite el valor para a: "))
+b=int(input("Digite el valor para a: "))
+multiplicacion=a*b
+print("la multiplicacion es: ", multiplicacion)
+
+a=int(input("Digite el valor para a: "))
+b=int(input("Digite el valor para a: "))
+division=a/b
+print("la division es: ", division)
+
+a=int(input("Digite el valor para a: "))
+b=int(input("Digite el valor para a: "))
+division=a%b
+print("la division es: ", division)
