7449d5a3005b137b102bffeade8866de48c4124f
RonaldGuativa <167368405+RonaldGuativa@users.noreply.github.com>
2024-04-25 07:55:20 -0500

    Update ejercicio1.py

--- ejerciciosSecuenciales/ejercicio1.py
+++ ejerciciosSecuenciales/ejercicio1.py
@@ -1,8 +1,12 @@
 #ingresar la informacion solicitada 
 
 nombre=(input("ingrese el nombre del estudiante:"))
+#ingresar la informacion solicitada 
 instituto=(input("ingrese el nombre del instuto:"))
+#ingresar la informacion solicitada 
 curso=(input("ingrese el nombre del curso:"))
+#ingresar la informacion solicitada 
+
 
 #muestra toda la informacion ingresada en orden 
 
