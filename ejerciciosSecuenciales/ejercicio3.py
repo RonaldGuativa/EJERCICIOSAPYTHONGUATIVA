daac10a6673d9d8f57813b6781daab0a57cf5fc5
RonaldGuativa <167368405+RonaldGuativa@users.noreply.github.com>
2024-04-25 08:01:41 -0500

    Update ejercicio3.py

--- ejerciciosSecuenciales/ejercicio3.py
+++ ejerciciosSecuenciales/ejercicio3.py
@@ -1,7 +1,8 @@
 #ingresar los numeros
-
 numero1=float(input("ingrese el numero 1 porfi"))
+#ingresar los numeros
 numero2=float(input("ingrese el numero 2 porfi"))
+#ingresar los numeros
 numero3=float(input("ingrese el numero 3 porfi"))
 
 #hacer la formula para promedio
