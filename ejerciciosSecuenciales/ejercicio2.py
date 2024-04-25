a91fb9a9182c823b2ef8a15cad822c9272461d53
RonaldGuativa <167368405+RonaldGuativa@users.noreply.github.com>
2024-04-25 08:00:18 -0500

    Update ejercicio2.py

--- ejerciciosSecuenciales/ejercicio2.py
+++ ejerciciosSecuenciales/ejercicio2.py
@@ -2,13 +2,15 @@
 #ingresar los valores en los lugares respectivos
 
 base=float(input("ingrese la base del rectangulo"))
+#ingresar el valor de la base
 altura=float(input("ingrese la altura del rectangulo"))
+#ingresar el valor de la altura 
 
 #hacer las formulas para perimetro y area
 
 perimetro= 2*(base+altura)
 area=(base*altura)
 
-##muestra toda la informacion ingresada en orden 
+#muestra toda la informacion ingresada en orden 
 
 print("El perimetro del rectangulo es",perimetro,"y la  del rectagunlo es", area )
