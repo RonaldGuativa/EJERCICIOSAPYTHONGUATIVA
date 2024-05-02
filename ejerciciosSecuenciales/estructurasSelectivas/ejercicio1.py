37d30d9dc9d454430844911864edfac9fc928917
RonaldGuativa <167368405+RonaldGuativa@users.noreply.github.com>
2024-05-02 07:25:26 -0500

    ejercicio1.py

--- ejerciciosSecuenciales/estructurasSelectivas/ejercicio1.py
+++ ejerciciosSecuenciales/estructurasSelectivas/ejercicio1.py
@@ -1,11 +1,11 @@
 
 
-
-#ingresar las notas
-
 nota1=float(input("ingrese la nota 1 porfi"))
 nota2=float(input("ingrese la nota 2 porfi"))
 nota3=float(input("ingrese la nota 3 porfi"))
+
+#de las lineas 3 a 5 se ingresan las notas para el promedio
+
 
 #hacer la formula para promedio para saber la nota 
 
@@ -15,8 +15,11 @@ promedio=(nota1+nota2+nota3)/3
 
 print("El promedio de los 3 numeros es",promedio,)
 
-#
+#se muestra en pantalla el resultado de la ecuacion
+
 if promedio >= 70:
     print(f"El aprendiz ha aprobado Algoritmia con un promedio de {promedio:.2f}")
 else:
     print(f"El aprendiz ha desaprobado Algoritmia con un promedio de {promedio:.2f}")
+
+#De las lineas 20 a 23 se muestra el mensaje respecto a la condicion que corresponda
