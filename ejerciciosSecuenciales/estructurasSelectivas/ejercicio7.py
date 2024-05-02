7d557cec80636ad587b625262dbbd3d0a6c38671
RonaldGuativa <167368405+RonaldGuativa@users.noreply.github.com>
2024-05-02 08:31:48 -0500

    ejercicio7.py

--- ejerciciosSecuenciales/estructurasSelectivas/ejercicio7.py
+++ ejerciciosSecuenciales/estructurasSelectivas/ejercicio7.py
@@ -1,7 +1,7 @@
 # Solicitar al usuario ingresar la cantidad de llantas a comprar
 cantidad_llantas = int(input("Ingrese la cantidad de llantas a comprar: "))
 
-# Definir los precios por cantidad de llantas según la promoción establecida
+# de las lineas 5 a 10  Definimos las condiciones con los precios por cantidad de llantas según la promoción establecida
 if cantidad_llantas < 5:
     precio_por_llanta = 300  # Precio por llanta si se compran menos de cinco
 elif cantidad_llantas >= 5 and cantidad_llantas <= 10:
@@ -12,7 +12,7 @@ else:
 # Calcular el precio total de las llantas
 precio_total = cantidad_llantas * precio_por_llanta
 
-# Imprimir el resultado
+# de la linea 16 a 18 mostramos el resultado segun las condiciones que corresponda 
 print(f"Cantidad de llantas a comprar: {cantidad_llantas}")
 print(f"Precio por llanta: ${precio_por_llanta}")
 print(f"Precio total a pagar: ${precio_total}")
